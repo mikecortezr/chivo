26ca95d31f8cf65bf8132abe201e22f3001017d3
Miguel Cortez <miguelcortez209@gmail.com>
2020-06-28 02:24:04 -0700

    Agregado archivos español

--- generated/code/Magento/Customer/Model/ResourceModel/GroupRepository/Interceptor.php
+++ generated/code/Magento/Customer/Model/ResourceModel/GroupRepository/Interceptor.php
@@ -26,4 +26,56 @@ class Interceptor extends \Magento\Customer\Model\ResourceModel\GroupRepository 
             return $this->___callPlugins('save', func_get_args(), $pluginInfo);
         }
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getById($id)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getById');
+        if (!$pluginInfo) {
+            return parent::getById($id);
+        } else {
+            return $this->___callPlugins('getById', func_get_args(), $pluginInfo);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getList(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getList');
+        if (!$pluginInfo) {
+            return parent::getList($searchCriteria);
+        } else {
+            return $this->___callPlugins('getList', func_get_args(), $pluginInfo);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete(\Magento\Customer\Api\Data\GroupInterface $group)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'delete');
+        if (!$pluginInfo) {
+            return parent::delete($group);
+        } else {
+            return $this->___callPlugins('delete', func_get_args(), $pluginInfo);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteById($id)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'deleteById');
+        if (!$pluginInfo) {
+            return parent::deleteById($id);
+        } else {
+            return $this->___callPlugins('deleteById', func_get_args(), $pluginInfo);
+        }
+    }
 }
